2028ff40d5678ab6e4da92f68ef44667fddedda1
Mikhail Borisov <mick@nsk.jetico.com>
2023-08-08 13:18:12 +0700

    code

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+const item = {
+  brand: 'brand',
+  title: 'title',
+  description: 'description',
+  descriptionFull: 'description full description full description full description full description full description full full description full',
+  price: 123.00,
+  currency: '$',
+}
+
+const ShopItemFunc = ({item}) => (
+  <>
+    <div class="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div class="description">{item.descriptionFull}</div>
+      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+      <div class="divider"></div>
+      <div class="purchase-info">
+        <div class="price">
+          {item.currency}{Number(item.price).toFixed(2)}
+          </div>
+          <button>Добавить в корзину</button>
+      </div>
+    </div>
+  </>
+)
+
+class ShopItemCl extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const {user} = this.props;
+        return(
+        <>
+            <div class="main-content">
+              <h2>{item.brand}</h2>
+              <h1>{item.title}</h1>
+              <h3>{item.description}</h3>
+              <div class="description">{item.descriptionFull}</div>
+              <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+              <div class="divider"></div>
+              <div class="purchase-info">
+                <div class="price">
+                  {item.currency}{Number(item.price).toFixed(2)}
+                  </div>
+                  <button>Добавить в корзину</button>
+              </div>
+            </div>
+        </>
+
+        )
+    }
+}
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemCl item={item} />
+
+        </div>
+      </div>
+      </>
+  );
+}
+
+export default App;
